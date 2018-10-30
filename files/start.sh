ddbf233bde7417888b54ccddb61e8ac1b19e446f
xin478 <xin478@126.com>
2018-10-30 15:18:10 +0800

    Update start.sh
    添加启动自动修改tracker

--- files/start.sh
+++ files/start.sh
@@ -8,6 +8,12 @@ fi
 if [ ! -f /conf/on-complete.sh ]; then
 	cp /conf-copy/on-complete.sh /conf/on-complete.sh
 fi
+list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
+    sed -i '$a bt-tracker='${list} /conf/aria2.conf
+else
+    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /conf/aria2.conf
+fi
 
 chmod +x /conf/on-complete.sh
 touch /conf/aria2.session
